414c03925a53d9d2f382f89c0ff1cc39ddc213de
hygorfe <hygordevcortes@gmail.com>
2024-11-25 19:21:30 -0300

    Second Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules } from '@angular/router';
-
 export const routes: Routes = [
   {
     path: 'profile',
